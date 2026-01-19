117076812d49cf509bf9e45d95a1bd1e091616f1
vcantarella <vcantarella@gmail.com>
2026-01-19 18:07:08 +0100

    export tags for the important functions.

--- src/nonlinearlstr.jl
+++ src/nonlinearlstr.jl
@@ -6,4 +6,7 @@ include("bounded_subproblems.jl")
 include("bounded_step.jl")
 include("algorithms.jl")
 include("bounded_algorithms.jl")
+
+export lm_trust_region
+export lm_trust_region_reflective
 end
